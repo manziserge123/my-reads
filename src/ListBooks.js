dd6b379a4fed458a622ac920bc10aaed81538bdd
Rahul Krishna <rkbalu524@gmail.com>
2017-07-07 15:00:58 +0530

    Feat: Add React Router and define two routes

---
+++ src/ListBooks.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class ListBooks extends React.Component{
+  render(){
+    return(
+      <div className="list-books-title">
+        <h1>MyReads</h1>
+      </div>
+    )
+  }
+}
+
+export default ListBooks
---
+++ src/Searchpage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Searchpage extends React.Component{
+  render(){
+    return(
+      <div>Search Page</div>
+    )
+  }
+}
+
+export default Searchpage
