fcdaef12b597d5390e0eaaa5f476b06bb78bafa1
Rahul Krishna <rkbalu524@gmail.com>
2017-07-07 15:54:22 +0530

    Feat: List the books from API under three different sections

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,10 +1,117 @@
 import React from 'react'
 
+
 class ListBooks extends React.Component{
   render(){
+    const currentlyReadingBooks=this.props.books.filter((book=>book.shelf==='currentlyReading')).map((book)=>{
+      return (
+        <li key={book.id}>
+          <div className="book">
+            <div className="book-top">
+              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+              <div className="book-shelf-changer">
+                <select>
+                  <option value="none" disabled>Move to...</option>
+                  <option value="currentlyReading">Currently Reading</option>
+                  <option value="wantToRead">Want to Read</option>
+                  <option value="read">Read</option>
+                  <option value="none">None</option>
+                </select>
+              </div>
+            </div>
+            <div className="book-title">{book.title}</div>
+            <div className="book-authors">{book.authors.map((author)=>{
+                return(
+                  <span key={author} className="author-name"> {author}</span>
+                )
+            })}</div>
+          </div>
+        </li>
+      )
+    })
+    const wantToReadBooks=this.props.books.filter((book=>book.shelf==='wantToRead')).map((book)=>{
+      return (
+        <li key={book.id}>
+          <div className="book">
+            <div className="book-top">
+              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+              <div className="book-shelf-changer">
+                <select>
+                  <option value="none" disabled>Move to...</option>
+                  <option value="currentlyReading">Currently Reading</option>
+                  <option value="wantToRead">Want to Read</option>
+                  <option value="read">Read</option>
+                  <option value="none">None</option>
+                </select>
+              </div>
+            </div>
+            <div className="book-title">{book.title}</div>
+            <div className="book-authors">{book.authors.map((author)=>{
+                return(
+                  <span key={author} className="author-name"> {author}</span>
+                )
+            })}</div>
+          </div>
+        </li>
+      )
+    })
+    const readBooks=this.props.books.filter((book=>book.shelf==='read')).map((book)=>{
+      return (
+        <li key={book.id}>
+          <div className="book">
+            <div className="book-top">
+              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+              <div className="book-shelf-changer">
+                <select>
+                  <option value="none" disabled>Move to...</option>
+                  <option value="currentlyReading">Currently Reading</option>
+                  <option value="wantToRead">Want to Read</option>
+                  <option value="read">Read</option>
+                  <option value="none">None</option>
+                </select>
+              </div>
+            </div>
+            <div className="book-title">{book.title}</div>
+            <div className="book-authors">{book.authors.map((author)=>{
+                return(
+                  <span key={author} className="author-name"> {author}</span>
+                )
+            })}</div>
+          </div>
+        </li>
+      )
+    })
     return(
-      <div className="list-books-title">
-        <h1>MyReads</h1>
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div className="bookshelf">
+            <h2 className="bookshelf-title">Currently Reading</h2>
+            <div className="bookshelf-books">
+              <ol className="books-grid">
+                {currentlyReadingBooks}
+              </ol>
+            </div>
+          </div>
+          <div className="bookshelf">
+            <h2 className="bookshelf-title">Want to Read</h2>
+            <div className="bookshelf-books">
+              <ol className="books-grid">
+                {wantToReadBooks}
+              </ol>
+            </div>
+          </div>
+          <div className="bookshelf">
+            <h2 className="bookshelf-title">Read</h2>
+            <div className="bookshelf-books">
+              <ol className="books-grid">
+                {readBooks}
+              </ol>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
